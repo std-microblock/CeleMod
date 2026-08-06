885d6e886f4d11f4df0febba1fd0f49f4fdff410
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2026-08-06 21:11:39 +0800

    ui: fix hmr

--- src/celemod-ui/vite.config.ts
+++ src/celemod-ui/vite.config.ts
@@ -1,8 +1,10 @@
 import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  plugins: [react()],
   resolve: {
     alias: {
       locales: resolve(__dirname, 'locales'),
